25e890276d8ef92110630b3c8aa57960dc093297
Alex <rengarsanj@gmail.com>
2023-04-11 13:10:19 +0300

    V2

--- src/schemas/index.js
+++
@@ -1,6 +0,0 @@
-import * as yup from "yup";
-
-export const basicSchema = yup.object().shape({
-  email: yup.string().email("Please provide a valid e-mail address").required("Email address is required"),
-  checkbox: yup.bool().oneOf([true], 'You must accept the terms and conditions').required('You must accept the terms and conditions'),
-});
